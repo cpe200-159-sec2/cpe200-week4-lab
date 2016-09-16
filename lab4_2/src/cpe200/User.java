773b7310eebe90f3c98483c4ffcf1eca779c7da0
WIWI <beam-303@hotmail.com>
2016-09-16 17:58:36 +0700

    lab4_2

--- lab4_2/src/cpe200/User.java
+++ lab4_2/src/cpe200/User.java
@@ -4,25 +4,40 @@ package cpe200;
  * Created by pruet on 6/9/2559.
  */
 public class User {
-    public String userName;
-    public String password;
+
+    private String userName;
+    private String password;
 
     public User() {
-    }
 
+    }
+   public User (String name ,String password){
+        this.userName = name;
+        this.password =password;
+
+}
     public boolean setUserName(String name) {
-        return false;
+
+        if ((name.length() >= 8) &&(name.matches("^([A-Za-z])([A-Za-z0-9]+)$"))){
+            userName =name;
+            return true;
+        }
+        else return false;
     }
 
     public boolean setPassword(String name) {
-        return false;
+        if((name.length() >=12)&&(name.matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])([a-zA-Z0-9]+)$"))){
+            password =name;
+            return true;
+        }
+        else    return false;
     }
 
     public String getUserName() {
-        return null;
+        return userName;
     }
 
     public String getPassword() {
-        return null;
+        return password;
     }
 }
