b5bc2edae5d4657cf9e4b50a96a8cfa969b2af3d
MisterMu <painter.suwannawat@gmail.com>
2016-09-16 15:50:56 +0700

    Complete Lab4_1

--- lab4_1/src/cpe200/BinaryCalculator.java
+++ lab4_1/src/cpe200/BinaryCalculator.java
@@ -1,39 +1,72 @@
 package cpe200;
 
 
+import java.util.AbstractCollection;
+
 public class BinaryCalculator {
-    public Operand firstOperand;
-    public Operand secondOperand;
+    private Operand firstOperand;
+    private Operand secondOperand;
+    private Double Num1, Num2;
 
     public BinaryCalculator() {
     }
 
     public void setFirstOperand(Operand operand) {
+        Num1 = Double.parseDouble(operand.getOperand());
     }
 
 
     public void setSecondOperand(Operand operand) {
+        Num2 = Double.parseDouble(operand.getOperand());
     }
 
     public String add() {
-        return null;
+        Double Ans = Num1 + Num2;
+        int tmp_int = Ans.intValue();
+        if (tmp_int == Ans)
+            return Integer.toString(tmp_int);
+        return Double.toString(Ans);
     }
 
     public String subtract() {
-        return null;
+        Double Ans = Num1 - Num2;
+        int tmp_int = Ans.intValue();
+        if (tmp_int == Ans)
+            return Integer.toString(tmp_int);
+        long tmp_long = Math.round(Ans*100000);
+        Ans = (double)tmp_long;
+        Ans /= 100000;
+        return Double.toString(Ans);
     }
 
     public String multiply() {
-        return null;
+        Double Ans = Num1 * Num2;
+        int tmp_int = Ans.intValue();
+        if (tmp_int == Ans)
+            return Integer.toString(tmp_int);
+        return Double.toString(Ans);
     }
 
     /* This method should throw an exception when divide by zero */
     public String division() throws ArithmeticException {
-        return null;
+        if (Num2 == 0)
+            throw new RuntimeException();
+        Double Ans = Num1 / Num2;
+        int tmp_int = Ans.intValue();
+        if (tmp_int == Ans)
+            return Integer.toString(tmp_int);
+        long tmp_long = Math.round(Ans*100000);
+        Ans = (double)tmp_long;
+        Ans /= 100000;
+        return Double.toString(Ans);
     }
 
     public String power() {
-        return null;
+        Double Ans = Math.pow(Num1, Num2);
+        int tmp_int = Ans.intValue();
+        if (tmp_int == Ans)
+            return Integer.toString(tmp_int);
+        return Double.toString(Ans);
     }
 
 }
