fb7fb37afb82269dcf0e74a97003aef620d5656b
_iDong <dd_dong.bk@hotmail.com>
2016-09-16 17:45:47 +0700

    ok

--- lab4_1/src/cpe200/Operand.java
+++ lab4_1/src/cpe200/Operand.java
@@ -1,18 +1,24 @@
 package cpe200;
 
 public class Operand {
-    public String operand;
+    private String operand;
 
     public Operand(String operand) {
         this.operand = operand;
     }
 
     public Operand(int operand) {
-
+        this.operand = Integer.toString(operand);
     }
 
     public Operand(double operand) {
-
+        this.operand = Double.toString(operand);
+    }
+    public void setOperand(String operand) {
+        this.operand = operand;
+    }
+    public  String getOperand(){
+        return this.operand;
     }
 
 }
