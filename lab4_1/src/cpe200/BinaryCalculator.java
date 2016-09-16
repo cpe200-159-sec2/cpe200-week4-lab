235a2e96fe5ab35435cf986587c868c154e8ecb1
nohajime <patchara0pt002@gmail.com>
2016-09-16 17:20:49 +0700

    Done

--- lab4_1/src/cpe200/BinaryCalculator.java
+++ lab4_1/src/cpe200/BinaryCalculator.java
@@ -2,38 +2,70 @@ package cpe200;
 
 
 public class BinaryCalculator {
-    public Operand firstOperand;
-    public Operand secondOperand;
+    private Operand firstOperand;
+    private Operand secondOperand;
+    private double firstNum,secondNum;
 
     public BinaryCalculator() {
+        firstOperand = new Operand('0');
+        secondOperand = new Operand('0');
     }
 
     public void setFirstOperand(Operand operand) {
+        this.firstOperand = operand;
+        firstNum = Double.parseDouble(firstOperand.getOperand());
     }
 
 
     public void setSecondOperand(Operand operand) {
+        this.secondOperand = operand;
+        secondNum = Double.parseDouble(secondOperand.getOperand());
     }
 
     public String add() {
-        return null;
+        String sum = new Double(firstNum+secondNum).toString();
+        return sum.indexOf(".") < 0 ? sum : sum.replaceAll("0*$","").replaceAll("\\.$","");
     }
 
     public String subtract() {
-        return null;
+        Double sumdb = firstNum-secondNum;
+        String sum = new Double(firstNum-secondNum).toString();
+        sum = String.format("%.5f",sumdb);
+
+        return sum.indexOf(".") < 0 ? sum : sum.replaceAll("0*$","").replaceAll("\\.$","");
     }
 
     public String multiply() {
-        return null;
+        String sum = new Double(firstNum*secondNum).toString();
+
+        return sum.indexOf(".") < 0 ? sum : sum.replaceAll("0*$","").replaceAll("\\.$","");
     }
 
     /* This method should throw an exception when divide by zero */
     public String division() throws ArithmeticException {
-        return null;
+        if(secondNum==0) throw new RuntimeException("ERROR");
+        Double sumdb = firstNum/secondNum;
+
+        String sum = new Double(firstNum/secondNum).toString();
+        sum = String.format("%.5f",sumdb);
+        return sum.indexOf(".") < 0 ? sum : sum.replaceAll("0*$","").replaceAll("\\.$","");
     }
 
     public String power() {
-        return null;
+        double power = 1;
+        for(int i=0;i<secondNum;i++){
+            power *= firstNum;
+        }
+        String sum = new Double(power).toString();
+
+        return sum.indexOf(".") < 0 ? sum : sum.replaceAll("0*$","").replaceAll("\\.$","");
     }
 
+    public Operand getFirstOperand() {
+        return firstOperand;
+    }
+
+    public Operand getSecondOperand() {
+        return secondOperand;
+    }
 }
