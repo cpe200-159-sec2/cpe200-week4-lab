d8b710cbef5703088c48dc3fff856f166a7fc11b
ASUS_kwpinthong <kw.pinthong@gmail.com>
2016-09-16 17:07:19 +0700

    Complete LAB1

--- lab4_1/src/cpe200/BinaryCalculator.java
+++ lab4_1/src/cpe200/BinaryCalculator.java
@@ -1,39 +1,60 @@
 package cpe200;
 
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class BinaryCalculator {
-    public Operand firstOperand;
-    public Operand secondOperand;
+    private Operand firstOperand;
+    private Operand secondOperand;
+
+
 
     public BinaryCalculator() {
     }
 
     public void setFirstOperand(Operand operand) {
+        firstOperand = operand;
     }
 
-
     public void setSecondOperand(Operand operand) {
+        secondOperand = operand;
     }
 
     public String add() {
-        return null;
+        BigDecimal one = new BigDecimal(firstOperand.getOperand());
+        BigDecimal two = new BigDecimal(secondOperand.getOperand());
+
+        return one.add(two).stripTrailingZeros().toString();
     }
 
     public String subtract() {
-        return null;
+        BigDecimal one = new BigDecimal(firstOperand.getOperand());
+        BigDecimal two = new BigDecimal(secondOperand.getOperand());
+
+        return one.subtract(two).stripTrailingZeros().toString();
     }
 
     public String multiply() {
-        return null;
+        BigDecimal one = new BigDecimal(firstOperand.getOperand());
+        BigDecimal two = new BigDecimal(secondOperand.getOperand());
+
+        return one.multiply(two).stripTrailingZeros().toString();
     }
 
     /* This method should throw an exception when divide by zero */
     public String division() throws ArithmeticException {
-        return null;
+        BigDecimal one = new BigDecimal(firstOperand.getOperand());
+        BigDecimal two = new BigDecimal(secondOperand.getOperand());
+
+        return one.divide(two,5, RoundingMode.HALF_UP).stripTrailingZeros().toString();
     }
 
     public String power() {
-        return null;
+        BigDecimal one = new BigDecimal(firstOperand.getOperand());
+        BigDecimal two = new BigDecimal(secondOperand.getOperand());
+
+        return one.pow(two.intValueExact()).stripTrailingZeros().toString();
     }
 
 }
