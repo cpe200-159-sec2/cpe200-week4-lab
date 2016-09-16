7ed14166d0d354390a9365dc273aea3162ea794b
MisterMu <painter.suwannawat@gmail.com>
2016-09-16 15:51:21 +0700

    Complete Lab4_2

--- lab4_2/src/cpe200/User.java
+++ lab4_2/src/cpe200/User.java
@@ -4,25 +4,55 @@ package cpe200;
  * Created by pruet on 6/9/2559.
  */
 public class User {
-    public String userName;
-    public String password;
+    private String userName;
+    private String password;
 
     public User() {
     }
 
     public boolean setUserName(String name) {
-        return false;
+        if (name.length() < 8)
+            return false;
+        if (name.charAt(0) >= '0' && name.charAt(0) <= '9')
+            return false;
+        for (short i = 0; i < name.length(); i++)
+        {
+            char tmp = name.charAt(i);
+            if (!((tmp >= 'A' && tmp <= 'Z') || (tmp >= 'a' && tmp <= 'z') || (tmp >= '0' && tmp <= '9')))
+                return false;
+        }
+        userName = name;
+        return true;
     }
 
     public boolean setPassword(String name) {
-        return false;
+        boolean chk1,chk2,chk3;
+        chk1 = chk2 = chk3 = false;
+        if (name.length() < 12)
+            return false;
+        for (short i = 0; i < name.length(); i++)
+        {
+            char tmp = name.charAt(i);
+            if (!((tmp >= 'A' && tmp <= 'Z') || (tmp >= 'a' && tmp <= 'z') || (tmp >= '0' && tmp <= '9')))
+                return false;
+            if (tmp >= 'A' && tmp <= 'Z')
+                chk1 = true;
+            else if (tmp >= 'a' && tmp <= 'z')
+                chk2 = true;
+            else if (tmp >= '0' && tmp <= '9')
+                chk3 = true;
+        }
+        if (!(chk1 && chk2 && chk3))
+            return false;
+        password = name;
+        return true;
     }
 
     public String getUserName() {
-        return null;
+        return userName;
     }
 
     public String getPassword() {
-        return null;
+        return password;
     }
 }
--- lab4_2/src/cpe200/Users.java
+++ lab4_2/src/cpe200/Users.java
@@ -8,42 +8,73 @@ import java.util.ArrayList;
 public class Users {
     public ArrayList<User> userList;
 
+    public Users()
+    {
+        userList = new ArrayList<User>();
+    }
+
     public void addUser(User user)
     {
+        userList.add(user);
+
     }
 
     public void addUser(String userName, String password)
     {
+        User u = new User();
+        u.setUserName(userName);
+        u.setPassword(password);
+        userList.add(u);
     }
 
     public void deleteUser(User user)
     {
-
+        userList.remove(user);
     }
 
     public boolean exists(User user)
     {
+        for (short i = 0; i < count(); i++)
+        {
+            if(userList.get(i) == user)
+                return true;
+        }
         return false;
     }
 
     public boolean usernameExists(String username)
     {
+        for (short i = 0; i < count(); i++)
+        {
+            if(userList.get(i).getUserName() == username)
+                return true;
+        }
         return false;
     }
 
     /* This method should return null when the user with username is not in the list */
     public User getUserByUsername(String userName)
     {
+        for (short i = 0; i < count(); i++)
+        {
+            if(userList.get(i).getUserName() == userName)
+                return userList.get(i);
+        }
         return null;
     }
 
     public int count()
     {
-        return 0;
+        return userList.size();
     }
 
     public User[] getUserArray()
     {
-        return null;
+        User u[] = new User[count()];
+        for (short i = 0; i < count(); i++)
+        {
+            u[i] = userList.get(i);
+        }
+        return u;
     }
 }
