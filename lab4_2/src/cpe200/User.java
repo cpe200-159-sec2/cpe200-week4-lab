2ca2ea4a798e050e80b5e8d76e1dc2d430a41844
defirstt <saranyapong1993@gmail.com>
2016-09-16 18:32:59 +0700

    finish lab4

--- lab4_2/src/cpe200/User.java
+++ lab4_2/src/cpe200/User.java
@@ -4,25 +4,38 @@ package cpe200;
  * Created by pruet on 6/9/2559.
  */
 public class User {
-    public String userName;
-    public String password;
+    private String userName;
+    private String password;
 
     public User() {
+        setUserName("");
+        setPassword("");
     }
 
     public boolean setUserName(String name) {
-        return false;
+        String regex = "^[a-zA-Z][A-Za-z0-9]{7,}";
+        if (name.matches(regex)) {
+            userName = name;
+            return true;
+        } else
+            return false;
     }
 
     public boolean setPassword(String name) {
-        return false;
+        String regex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]{12,}$";
+        if (name.matches(regex)) {
+            password = name;
+            return true;
+
+        } else
+            return false;
     }
 
     public String getUserName() {
-        return null;
+        return userName;
     }
 
     public String getPassword() {
-        return null;
+        return password;
     }
 }
