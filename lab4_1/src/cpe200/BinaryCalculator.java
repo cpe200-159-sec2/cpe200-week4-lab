7027f9f0818b65dd9924e2ef02481df3847ca228
Ngamsai <ngamsiri.bm@gmail.com>
2016-09-16 18:19:45 +0700

    Complete Lab 4

--- lab4_1/src/cpe200/BinaryCalculator.java
+++ lab4_1/src/cpe200/BinaryCalculator.java
@@ -1,39 +1,64 @@
 package cpe200;
 
 
+import java.math.BigDecimal;
+
+
 public class BinaryCalculator {
-    public Operand firstOperand;
-    public Operand secondOperand;
+    private Operand firstOperand;
+    private Operand secondOperand;
+    private Operand operand;
+   // private Double first,second,result;
+    private BigDecimal first , second ,result;
 
     public BinaryCalculator() {
+        setFirstOperand(new Operand(0));
+        setSecondOperand(new Operand(0));
     }
 
     public void setFirstOperand(Operand operand) {
+        first = new BigDecimal(operand.getOperand());
     }
 
 
     public void setSecondOperand(Operand operand) {
+        second = new BigDecimal(operand.getOperand());
     }
 
     public String add() {
-        return null;
+        result = first.add(second).stripTrailingZeros();
+        return result.toString();
     }
 
     public String subtract() {
-        return null;
+        result = first.subtract(second).stripTrailingZeros();
+        return result.toString();
     }
 
     public String multiply() {
-        return null;
+        result = first.multiply(second).stripTrailingZeros();
+        return result.toString();
     }
 
     /* This method should throw an exception when divide by zero */
     public String division() throws ArithmeticException {
-        return null;
+        if (second.equals(0)){
+            throw new RuntimeException("0");
+        }else {
+           return first.divide(second,5,BigDecimal.ROUND_UP).stripTrailingZeros().toString();
+        }
     }
 
     public String power() {
-        return null;
+        result = first.pow(second.intValue()).stripTrailingZeros();
+        return result.toString();
     }
 
+    public Operand getFirstOperand() {
+        return firstOperand;
+    }
+
+    public Operand getSecondOperand() {
+        return secondOperand;
+    }
 }
