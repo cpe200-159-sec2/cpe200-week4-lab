f8ae3075424d42e4699e3894cef2eefc9938af40
Phakinchu <phakinchu@gmail.com>
2016-09-16 17:58:34 +0700

    580610664

--- lab4_2/src/cpe200/User.java
+++ lab4_2/src/cpe200/User.java
@@ -4,25 +4,42 @@ package cpe200;
  * Created by pruet on 6/9/2559.
  */
 public class User {
-    public String userName;
-    public String password;
+    private String userName;
+    private String password;
 
     public User() {
+        //เว้นววรคเพื่อความสวยงาม
+    }
+
+    public User(String userName , String password){
+
+        setUserName(userName);
+        setPassword(password) ;
     }
 
     public boolean setUserName(String name) {
+        String check = "^[a-zA-Z][a-zA-Z0-9]{7,}$" ;
+        if(name.matches(check)){
+            userName=check ;
+            return true ;
+        }
         return false;
     }
 
     public boolean setPassword(String name) {
+        String check = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])[A-Za-z0-9]{12,}$";
+        if(name.matches(check)){
+            password = check ;
+            return  true ;
+        }
         return false;
     }
 
     public String getUserName() {
-        return null;
+        return userName;
     }
 
     public String getPassword() {
-        return null;
+        return password;
     }
 }
