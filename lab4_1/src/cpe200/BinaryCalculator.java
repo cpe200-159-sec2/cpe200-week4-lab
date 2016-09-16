bac6a9dc450e7e5611f78823c46c3aaad40b4b97
Kraiwich2015 <kraiwich2015@gmail.com>
2016-09-16 18:03:32 +0700

    580610620

--- lab4_1/src/cpe200/BinaryCalculator.java
+++ lab4_1/src/cpe200/BinaryCalculator.java
@@ -1,39 +1,42 @@
 package cpe200;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class BinaryCalculator {
-    public Operand firstOperand;
-    public Operand secondOperand;
+    private BigDecimal firstOperand,secondOperand;
 
     public BinaryCalculator() {
     }
 
     public void setFirstOperand(Operand operand) {
+        firstOperand = new BigDecimal(operand.getOperand());
     }
 
 
     public void setSecondOperand(Operand operand) {
+        secondOperand = new BigDecimal(operand.getOperand());
     }
 
     public String add() {
-        return null;
+        return firstOperand.add(secondOperand).stripTrailingZeros().toString();
     }
 
     public String subtract() {
-        return null;
+        return firstOperand.subtract(secondOperand).stripTrailingZeros().toString();
     }
 
     public String multiply() {
-        return null;
+        return firstOperand.multiply(secondOperand).stripTrailingZeros().toString();
     }
 
     /* This method should throw an exception when divide by zero */
     public String division() throws ArithmeticException {
-        return null;
+        return firstOperand.divide(secondOperand,5, RoundingMode.HALF_UP).stripTrailingZeros().toString();
     }
 
     public String power() {
-        return null;
+        return firstOperand.pow(secondOperand.intValue()).stripTrailingZeros().toString();
     }
 
 }
