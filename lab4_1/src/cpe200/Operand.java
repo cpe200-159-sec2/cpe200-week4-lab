9ac30fac4c9a94046d84aa010695990215244f28
Panatchakorn <panatchakorn4774@gmail.com>
2016-09-16 18:30:16 +0700

    Lab4 Completed

--- lab4_1/src/cpe200/Operand.java
+++ lab4_1/src/cpe200/Operand.java
@@ -1,18 +1,28 @@
 package cpe200;
 
+import com.sun.org.apache.xml.internal.dtm.ref.DTMChildIterNodeList;
+
 public class Operand {
-    public String operand;
+    private String operand;
 
     public Operand(String operand) {
+
         this.operand = operand;
     }
 
     public Operand(int operand) {
 
+        this.operand = Integer.toString(operand);
     }
 
     public Operand(double operand) {
 
+        this.operand = Double.toString(operand);
+    }
+
+    public String getOperand() {
+
+        return operand;
     }
 
 }
--- lab4_2/src/cpe200/User.java
+++ lab4_2/src/cpe200/User.java
@@ -4,25 +4,37 @@ package cpe200;
  * Created by pruet on 6/9/2559.
  */
 public class User {
-    public String userName;
-    public String password;
+    private String userName;
+    private String password;
 
     public User() {
+        setUserName("");
+        setPassword("");
     }
 
-    public boolean setUserName(String name) {
+    public boolean setUserName(String userName) {
+        if (userName.matches("^[A-Za-z][A-Za-z0-9]{7,}$")){
+            this.userName = userName;
+            return true;
+        }
         return false;
     }
 
-    public boolean setPassword(String name) {
+    public boolean setPassword(String password) {
+        if (password.matches("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{12,}$")){
+            this.password = password;
+            return true;
+        }
         return false;
     }
 
     public String getUserName() {
-        return null;
+
+        return userName;
     }
 
     public String getPassword() {
-        return null;
+
+        return password;
     }
 }
