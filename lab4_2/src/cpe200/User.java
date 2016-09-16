c260ac08833642ad05273e3046dcde0ae0961044
totaythatchayut <thatchayut.tay@gmail.com>
2016-09-16 17:40:41 +0700

    Lab1 : DONE Lab2 : DONE

--- lab4_2/src/cpe200/User.java
+++ lab4_2/src/cpe200/User.java
@@ -4,25 +4,39 @@ package cpe200;
  * Created by pruet on 6/9/2559.
  */
 public class User {
-    public String userName;
-    public String password;
+    private String userName;
+    private String password;
 
     public User() {
+
     }
 
     public boolean setUserName(String name) {
-        return false;
+        String pattern="^([A-Za-z])([A-Za-z0-9]{7,})$";
+        if(name.matches(pattern))
+        {
+            userName=name;
+            return true;
+        }
+        else return false;
     }
 
     public boolean setPassword(String name) {
-        return false;
+        String pass="^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])([A-Za-z0-9]{12,})$";
+        if(name.matches(pass))
+        {
+            password=name;
+            return true;
+        }
+        else return false;
     }
 
     public String getUserName() {
-        return null;
+        return userName;
     }
 
+
     public String getPassword() {
-        return null;
+        return password;
     }
 }
