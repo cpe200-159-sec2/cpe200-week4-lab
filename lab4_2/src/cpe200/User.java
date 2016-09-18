d4e5be7dfbf0c0e33e4fd1d62cf83340a0ebc87c
nanearnano <nanearnano@gmail.com>
2016-09-18 23:20:42 +0700

    4-2 finish

--- lab4_2/src/cpe200/User.java
+++ lab4_2/src/cpe200/User.java
@@ -4,25 +4,37 @@ package cpe200;
  * Created by pruet on 6/9/2559.
  */
 public class User {
-    public String userName;
-    public String password;
+    private String userName;
+    private String password;
 
     public User() {
+        setPassword("");
+        setUserName("");
     }
 
     public boolean setUserName(String name) {
+        String pattern="^([A-Za-z])([A-Za-z0-9]){7,}$";
+        if(name.matches(pattern)){
+            userName=name;
+            return true;
+        }
         return false;
     }
 
     public boolean setPassword(String name) {
+        String pattern1="^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])([A-Za-z0-9]){12,}$";
+        if(name.matches(pattern1)){
+            password=name;
+            return  true;
+        }
         return false;
     }
 
     public String getUserName() {
-        return null;
+        return userName;
     }
 
     public String getPassword() {
-        return null;
+        return password;
     }
 }
