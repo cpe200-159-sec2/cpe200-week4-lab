77dddf70cff6bdeeaa81691ddfa0cbc23723847e
nanearnano <nanearnano@gmail.com>
2016-09-18 21:07:05 +0700

    4-1 finish

--- lab4_1/src/cpe200/BinaryCalculator.java
+++ lab4_1/src/cpe200/BinaryCalculator.java
@@ -1,39 +1,65 @@
 package cpe200;
 
 
+import java.math.BigDecimal;
+
 public class BinaryCalculator {
-    public Operand firstOperand;
-    public Operand secondOperand;
+    private Operand firstOperand;
+    private Operand secondOperand;
+    private BigDecimal first,second,sum,x;
 
     public BinaryCalculator() {
+        Operand firstOperand=new Operand(0);
+        Operand secondOperand=new Operand(0);
     }
 
     public void setFirstOperand(Operand operand) {
+        first=new BigDecimal(operand.getOperand());
+
     }
 
 
     public void setSecondOperand(Operand operand) {
+        second=new BigDecimal(operand.getOperand());
     }
 
     public String add() {
-        return null;
+        sum=first.add(second).stripTrailingZeros();
+        return sum.toString();
     }
 
     public String subtract() {
-        return null;
+        sum=first.subtract(second).stripTrailingZeros();
+        return sum.toString();
     }
 
     public String multiply() {
-        return null;
+        sum=first.multiply(second).stripTrailingZeros();
+        return sum.toString();
     }
 
     /* This method should throw an exception when divide by zero */
     public String division() throws ArithmeticException {
-        return null;
+        if (second.equals(0)) {
+            throw new RuntimeException("0");
+        } else {
+            return first.divide(second, 5, BigDecimal.ROUND_UP).stripTrailingZeros().toString();
+
+        }
+
     }
 
     public String power() {
-        return null;
+
+        sum=first.pow(second.intValueExact()).setScale(0);
+        return sum.toString();
     }
 
+    public Operand getFirstOperand() {
+        return firstOperand;
+    }
+
+    public Operand getSecondOperand() {
+        return secondOperand;
+    }
 }
