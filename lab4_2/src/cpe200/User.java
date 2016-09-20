b3f0131fe81ebf54c0a46dacd659c819a4cbd012
A550J <nice.dark@hotmail.com>
2016-09-20 10:32:23 +0700

    finished lab4_2

--- lab4_2/src/cpe200/User.java
+++ lab4_2/src/cpe200/User.java
@@ -11,18 +11,32 @@ public class User {
     }
 
     public boolean setUserName(String name) {
-        return false;
+        String regex = "^[A-Za-z][a-zA-Z0-9]{7,}$";
+        if(name.matches(regex)) {
+            userName = name;
+            return true;
+        }
+        else return false;
+
     }
 
     public boolean setPassword(String name) {
-        return false;
+        String check = "^((?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]))([A-Za-z0-9]){12,}$";
+        if (name.matches(check)) {
+            password = name;
+            return true;
+        }
+        else return false;
+
     }
 
     public String getUserName() {
-        return null;
+
+        return userName;
     }
 
     public String getPassword() {
-        return null;
+
+        return password;
     }
 }
