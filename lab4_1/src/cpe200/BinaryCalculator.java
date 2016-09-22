7457fcbcb9dacb08b9fe70e3993722f8dee0dfbb
Pair <roybura.b@gmail.com>
2016-09-23 00:33:25 +0700

    lab 4

--- lab4_1/src/cpe200/BinaryCalculator.java
+++ lab4_1/src/cpe200/BinaryCalculator.java
@@ -1,39 +1,52 @@
 package cpe200;
 
 
+import java.math.BigDecimal;
+
 public class BinaryCalculator {
-    public Operand firstOperand;
-    public Operand secondOperand;
+    private BigDecimal firstOperand;
+    private BigDecimal secondOperand;
 
     public BinaryCalculator() {
+        firstOperand = new BigDecimal(0);
+        secondOperand = new BigDecimal(0);
     }
 
     public void setFirstOperand(Operand operand) {
+        firstOperand = new BigDecimal(operand.getOperand());
     }
 
-
     public void setSecondOperand(Operand operand) {
+        secondOperand = new BigDecimal(operand.getOperand());
     }
 
     public String add() {
-        return null;
+        firstOperand = firstOperand.add(secondOperand).stripTrailingZeros();
+        return firstOperand.toString();
     }
 
     public String subtract() {
-        return null;
+        firstOperand = firstOperand.subtract(secondOperand).stripTrailingZeros();
+        return firstOperand.toString();
     }
 
     public String multiply() {
-        return null;
+        firstOperand = firstOperand.multiply(secondOperand).stripTrailingZeros();
+        return firstOperand.toString();
     }
-
     /* This method should throw an exception when divide by zero */
     public String division() throws ArithmeticException {
-        return null;
+        if(secondOperand.equals(0)){
+            throw new RuntimeException();
+        }
+        firstOperand = firstOperand.divide(secondOperand,5,BigDecimal.ROUND_HALF_UP).stripTrailingZeros();
+             return firstOperand.toString();
     }
 
     public String power() {
-        return null;
+        firstOperand = firstOperand.pow(secondOperand.intValue()).stripTrailingZeros();
+        return firstOperand.toString();
     }
+
 
 }
