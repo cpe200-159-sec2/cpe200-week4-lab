c2bb3637f906a43563fb91a04b6f03bfd961d8c0
PoommipatSe <poommipat.setthakorn@gmail.com>
2016-09-30 13:56:46 +0700

    Update User.java

--- lab4_2/src/cpe200/User.java
+++ lab4_2/src/cpe200/User.java
@@ -4,25 +4,38 @@ package cpe200;
  * Created by pruet on 6/9/2559.
  */
 public class User {
-    public String userName;
-    public String password;
+    private String userName;
+    private String password;
+    protected String USER_NAME_PATTERN = "^([A-Za-z])([A-Za-z0-9]{7,})$";
+    protected String PASSWORD_PATTERN = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])([A-Za-z0-9]){12,}$";
 
     public User() {
     }
 
     public boolean setUserName(String name) {
-        return false;
+        if (name.matches(USER_NAME_PATTERN)) {
+            this.userName = name;
+            return true;
+        } else
+            return false;
     }
 
-    public boolean setPassword(String name) {
-        return false;
+    public boolean setPassword(String password) {
+        if(password.matches(PASSWORD_PATTERN)) {
+            this.password = password;
+            return true;
+        }
+        else
+            return false;
     }
 
     public String getUserName() {
-        return null;
+
+        return this.userName;
     }
 
     public String getPassword() {
-        return null;
+
+        return this.password;
     }
 }
